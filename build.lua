3ef7cd1196a6b8971583d1ed71d50cb8bc650ac8
Joseph Wright <joseph.wright@morningstar2.co.uk>
2016-12-01 16:20:15 +0000

    Enable auto-versioning

--- build.lua
+++ build.lua
@@ -23,6 +23,24 @@ packtdszip  = true
 -- No tests for this bundle
 testfiledir = ""
 
+-- Detail how to set the version automatically
+versionfiles = {"*.def", "*.sty", "*.tex"}
+function setversion_update_line(line, date, release)
+  local date = string.gsub(date, "%-", "/")
+  -- Code
+  if string.match(line,  "^  %[%d%d%d%d/%d%d/%d%d") then
+    line = string.gsub(line, "%d%d%d%d/%d%d/%d%d [^ ]*", date ..  " " .. release)
+  end
+  -- Docs
+  if string.match(line,  "^  date=") then
+    line = string.gsub(line, "{[^}]*}", "{" .. date .."}")
+  end
+  if string.match(line,  "^  revision=") then
+    line = string.gsub(line, "{[^}]*}", "{" .. release .."}")
+  end
+  return line
+end
+
 -- Find and run the build system
 kpse.set_program_name ("kpsewhich")
 dofile (kpse.lookup ("l3build.lua"))
